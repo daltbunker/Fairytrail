6b9efb191ad1e11c1a82c91cb558d9aac421b83a
Dalton Bunker <dbbunker3@gmail.com>
2021-12-11 12:06:12 -0700

    added key to aboutCards

--- data/users.js
+++ data/users.js
@@ -8,22 +8,31 @@ const users = {
     character: 'Aladdin',
     aboutCards: [
       [
+        '101',
         'Most adventurous experience:',
         'My friend and i got stranded in a blizzard when our SUV froze in Iceland!',
         'black',
       ],
       [
+        '322',
         'Top Wishes:',
         'Take a year off to explore the world. Have a family. Create a better life for people around me.',
         'black',
       ],
       [
+        '109',
         'My Values ...',
         'Kindness, family, healthy living, and education.',
         '#7EBB89',
       ],
-      ['My Hobbies ...', 'Scuba diving and international cuisine', '#7EBB89'],
       [
+        '398',
+        'My Hobbies ...',
+        'Scuba diving and international cuisine',
+        '#7EBB89',
+      ],
+      [
+        '345',
         'Instagram (only shown to your adventure matches)',
         '@taigaeir',
         '#7EBB89',
