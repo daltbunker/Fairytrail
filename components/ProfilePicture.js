98132e3b9c41436075c298a912d92d2084f19915
Dalton Bunker <dbbunker3@gmail.com>
2021-12-11 15:39:18 -0700

    added horizonal scroll view to ProfilePicture

--- components/ProfilePicture.js
+++ components/ProfilePicture.js
@@ -1,35 +1,88 @@
-import React from 'react';
-import {View, Text, StyleSheet, ImageBackground} from 'react-native';
-import profile_1 from '../assets/images/profile-1.jpg';
+import React, {Component} from 'react';
+import {
+  View,
+  Dimensions,
+  StyleSheet,
+  Image,
+  ScrollView,
+  Text,
+} from 'react-native';
 
-const ProfilePicture = () => {
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={profile_1}
-        resizeMode="cover"
-        style={styles.image}>
+const images = [
+  require('../assets/images/profile-1.jpg'),
+  require('../assets/images/profile-2.jpg'),
+  require('../assets/images/profile-3.jpg'),
+];
+
+class PictureList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      current: 0,
+    };
+  }
+
+  onSlideSwiped = ({nativeEvent}) => {
+    const newCurrent = Math.ceil(
+      nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width,
+    );
+    this.setState(prevState => {
+      if (prevState !== newCurrent) {
+        return {current: newCurrent};
+      }
+    });
+  };
+
+  render() {
+    const imgArray = images.map((image, i) => {
+      return <Image key={i} source={image} style={styles.image} />;
+    });
+
+    const indicatorArray = images.map((image, i) => (
+      <Text
+        key={i}
+        style={{
+          color: i === this.state.current ? '#ffffff' : '#ffffff66',
+          margin: 4,
+        }}>
+        &#11044;
+      </Text>
+    ));
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          horizontal
+          pagingEnabled
+          onScroll={this.onSlideSwiped}
+          showsHorizontalScrollIndicator={false}>
+          {imgArray}
+        </ScrollView>
         <View style={styles.tag}>
-          <Text style={styles.text}>Paid</Text>
+          <Text style={styles.tagText}>Paid</Text>
         </View>
-      </ImageBackground>
-    </View>
-  );
-};
+        <View style={styles.scrollIndicator}>{indicatorArray}</View>
+      </View>
+    );
+  }
+}
+
+const {width, height} = Dimensions.get('window');
+const imgWidth = width - 40; // 40 repesents padding on screen view
+const imgHeight = height * 0.4;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    height: 450,
-    overflow: 'hidden',
-    borderRadius: 4,
     marginBottom: 30,
   },
   image: {
-    flex: 1,
+    height: imgHeight,
+    width: imgWidth,
+    resizeMode: 'cover',
   },
   tag: {
+    position: 'absolute',
     backgroundColor: '#E53B90',
+    top: 0,
     alignItems: 'center',
     justifyContent: 'center',
     height: 40,
@@ -37,11 +90,22 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
     borderBottomRightRadius: 20,
   },
-  text: {
+  tagText: {
     color: 'white',
     fontSize: 16,
     fontWeight: 'bold',
   },
+  scrollIndicator: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 10,
+    alignSelf: 'center',
+  },
+  circle: {
+    borderRadius: 60 / 2,
+    borderColor: 'white',
+    borderWidth: 3,
+  },
 });
 
-export default ProfilePicture;
+export default PictureList;
